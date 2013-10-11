b8bb61f045a973eba26851909ff7e4590e99f8ae
lukeschlangen <lukeschlangen@gmail.com>
2013-10-11 17:06:16 -0500

    Adding Main Menu Box
    Not pretty, needs a lot of work, but gives us something to start with?

--- ProjectDelta/ProjectDelta/Home/HomeText.cs
+++ ProjectDelta/ProjectDelta/Home/HomeText.cs
@@ -31,6 +31,12 @@ namespace ProjectDelta
 
         string world = "";
 
+        private Texture2D mainMenuBox;
+
+        private Vector2 mainMenuBoxPosition;
+
+
+
         public HomeText(float scale)
         {
             this.scale = scale;
@@ -40,6 +46,8 @@ namespace ProjectDelta
         {
             font = content.Load<SpriteFont>("large_input_font");
             worldFontPosition = new Vector2(375 * scale, 660 * scale);
+            mainMenuBox = content.Load<Texture2D>("home/main_menu");
+            mainMenuBoxPosition = new Vector2((1366 / 2 - mainMenuBox.Width * scale / 2), (768 / 2 - mainMenuBox.Height * scale / 2)); //hardcoded values for screenwidth and screenheight need to be replaced
         }
 
         public void Update(String whereTo)
@@ -50,6 +58,7 @@ namespace ProjectDelta
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.DrawString(font, world, worldFontPosition, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(mainMenuBox, mainMenuBoxPosition, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
         }
 
     }
